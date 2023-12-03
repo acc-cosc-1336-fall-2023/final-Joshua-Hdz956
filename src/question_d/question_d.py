14fd9e41305c299e91cd12ea9c330b96a0145038
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-12-03 18:38:57 +0000

    QuestionD and TestCase Joshua-Hdz956

---
+++ src/question_d/question_d.py
@@ -0,0 +1,24 @@
+#note to self- string1 must be 8-16 character, string2 must be equal to 4
+def get_most_likely_ancestor_consensus(string1, string2):
+    index_value = []
+    for i in range(len(string1)-len(string2)+1):
+        if string1[i:i+len(string2)] == string2:
+            index_value.append(i+1)
+    
+    return tuple(index_value)
+
+def validate_string_length(string1):
+    if len(string1) < 8 or len(string1) > 16:
+        return 'Invalid'
+
+def validate_substring_length(string2):
+    if len(string2) != 4:
+        return 'Invalid'
+
+
+
+
+
+
+
+
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,12 +1,22 @@
-#write function tests here, don't add input('') statements here!
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_d.question_d import get_most_likely_ancestor_consensus
 
 class Test_Config(unittest.TestCase):
 
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
+    def test_question_d(self):
+        positions = get_most_likely_ancestor_consensus("GATATATGCATATACTT", "ATAT")
+        x = positions[0]
+        y = positions[1]
+        z = positions[2]
+        self.assertEqual(x, 2)
+        print(x)
+        self.assertEqual(y, 4)
+        print(y)
+        self.assertEqual(z, 10)
+        print(z)
+        self.assertEqual(positions, (2,4,10))
+        print(positions)
 
 
